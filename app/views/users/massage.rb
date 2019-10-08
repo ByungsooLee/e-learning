b50524e01f46d4ae6ca3457a25935a54fd749dbe
이병수 <workout4050@gmail.com>
2019-10-08 10:30:47 +0800

    [Layout]Create Sign Up

---
+++ app/views/users/massage.rb
@@ -0,0 +1,5 @@
+class Message < ApplicationRecord
+    belongs_to :user
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 140 }
+  end
