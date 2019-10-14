dfe13ba22f07693e9e948eacc5a81076e66604ae
이병수 <workout4050@gmail.com>
2019-10-14 23:35:43 +0800

    [function] Admin Add Category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,4 +6,18 @@ class Admin::CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(categories_params)
+      if @category.save
+        redirect_to admin_categories_url
+      else
+        render "new"
+      end   
+  end
+
+  private
+  def categories_params
+    params.require(:category).permit(:title,:description)
+  end
 end
