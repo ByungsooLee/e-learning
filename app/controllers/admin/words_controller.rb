3987141ca227d9d3d3bc4ab9e483048fa2735222
이병수 <workout4050@gmail.com>
2019-10-15 22:40:09 +0800

    [Layout]Create words

---
+++ app/controllers/admin/words_controller.rb
@@ -0,0 +1,11 @@
+class Admin::WordsController < ApplicationController
+  def index
+    @category = Category.find(params[:category_id])
+  end
+
+  def new
+    @category = Category.find(params[:category_id])
+    @word = @category.words.new
+    3.times { @word.choices.new }
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
     validates :title,presence:true,length:{ maximum:50 }
     validates :description,presence:true,length:{ maximum:225 }
+    has_many :words
 end
