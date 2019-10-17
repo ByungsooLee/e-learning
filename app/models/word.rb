17838f25e8ae5cbd0a1270a272a21d9c374ff84f
ByungsooLee <41956738+ByungsooLee@users.noreply.github.com>
2019-10-17 09:37:03 +0800

    create words (#21)

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,23 @@
 class Word < ApplicationRecord
-    belongs_to :category
-    has_many :choices
+  has_many :choices, dependent: :destroy
+  belongs_to :category
+  accepts_nested_attributes_for :choices
+
+  validates :content, presence: true, length: {maximum: 50}
+  validate :has_one_correct
+
+  def correct_answer
+    choices.find_by(is_correct: true)
+    #このメソッドは@word燃えるの中にあるので、
+    #使いたい場合は@word = ~~と指定してあるから
+    #@ward.correct_answer.contentで使用できる。
+  end
+
+  private
+    def has_one_correct
+      unless choices.collect { |choice| choice.is_correct?}.count(true) == 1
+        errors.add(:choices,"Should have 1 correct answer")
+      end
+    end
 end
+
