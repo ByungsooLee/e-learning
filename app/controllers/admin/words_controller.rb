6a431944adead4626289e5ada2566edf143969f6
이병수 <workout4050@gmail.com>
2019-10-17 09:29:39 +0800

    create words

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,11 +1,27 @@
 class Admin::WordsController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
+    @words = @category.words.paginate(page: params[:page], per_page: 5)
   end
 
   def new
     @category = Category.find(params[:category_id])
     @word = @category.words.new
-    3.times { @word.choices.new }
+    3.times { @word.choices.build }
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @word = @category.words.new(word_params)
+    if @word.save
+      redirect_to admin_category_words_url
+    else
+      render "new"
+    end
+  end
+
+  private
+  def word_params
+    params.require(:word).permit(:content, choices_attributes: [:id, :content, :is_correct])
   end
 end
