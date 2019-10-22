382569a05706b7593125a7d7f94496e5083e4d1a
이병수 <workout4050@gmail.com>
2019-10-22 10:04:14 +0800

    correct gategory model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,6 @@
 class Category < ApplicationRecord
     validates :title,presence:true,length:{ maximum:50 }
     validates :description,presence:true,length:{ maximum:225 }
-    has_many :words
-    has_many :lessons
-    dependent :destroy
-    has_many :words
-    has_meny :lessons
+    has_many :words, dependent: :destroy
+    has_many :lessons, dependent: :destroy
 end
