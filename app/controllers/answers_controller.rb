811c3dea9b5543da0208a3ea64003355bc7be493
ByungsooLee <41956738+ByungsooLee@users.noreply.github.com>
2019-10-22 10:04:56 +0800

    [function] Lesson answering (#25)
    * [function] Lesson answering
    
    * add models
    
    * correct gategory model

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,28 @@
 class AnswersController < ApplicationController
 
   def new
+    @lesson = Lesson.find(params[:lesson_id])
+    @category = @lesson.category
+
+    if @lesson.next_word.nil?
+      redirect_to lesson_url(@lesson)
+    else
+      @answer = @lesson.answers.new
+    end
   end
 
+  def create
+    @lesson = Lesson.find(params[:lesson_id])
+    @answer = @lesson.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to new_lesson_answer_url(@lesson)
+    end
+
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:word_id, :lesson_id, :choice_id)
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
     validates :title,presence:true,length:{ maximum:50 }
     validates :description,presence:true,length:{ maximum:225 }
-    has_many :words
+    has_many :words, dependent: :destroy
+    has_many :lessons, dependent: :destroy
 end
